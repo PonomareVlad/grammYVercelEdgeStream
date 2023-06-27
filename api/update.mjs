a81b4279ce9c446ce21527835da88f876c766060
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-06-28 02:26:32 +0500

    Updated template

--- api/update.mjs
+++ api/update.mjs
@@ -1,9 +1,10 @@
-import {bot} from "../src/bot.mjs";
-import {secretToken} from "../src/data.mjs";
 import {webhookStream} from "vercel-grammy";
+import {bot, secretToken} from "../src/bot.mjs";
 
 export const config = {runtime: "edge"};
 
+// Default grammY handler for incoming updates via webhooks
 export default webhookStream(bot, {
+    timeoutMilliseconds: 29_000,
     secretToken,
 });
