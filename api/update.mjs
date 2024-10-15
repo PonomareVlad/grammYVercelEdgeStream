67df2bedf89bd2fa94a83857facc6543c16c7f34
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2024-10-15 04:38:59 +0200

    Update update.mjs

--- api/update.mjs
+++ api/update.mjs
@@ -1,7 +1,7 @@
 import {bot, secretToken} from "../src/bot.mjs";
 import {webhookStream} from "vercel-grammy";
 
-export const config = {runtime: "edge"};
+// export const config = {runtime: "edge"};
 
 // Custom grammY handler for incoming updates via webhooks with streaming response
 export const POST = webhookStream(bot, {
