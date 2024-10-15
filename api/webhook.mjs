704eebfd3d96fbbf5dbe3c94f0c48b6fc9e91b2a
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2024-10-15 04:39:38 +0200

    Update webhook.mjs

--- api/webhook.mjs
+++ api/webhook.mjs
@@ -1,7 +1,7 @@
 import {bot, secretToken} from "../src/bot.mjs";
 import {setWebhookCallback} from "vercel-grammy";
 
-export const config = {runtime: "edge"};
+// export const config = {runtime: "edge"};
 
 // Handler to set webhook url based on request headers
 export const POST = setWebhookCallback(bot, {
